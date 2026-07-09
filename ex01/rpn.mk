187229b78c925b7fcc2480f9add05ddfb115027b
Lea Devoudelle-Royer <ldevoude@z1r9p4.42lyon.fr>
2026-07-09 16:44:40 +0200

    feat(ex01): created the main, makefile and necessary folders to the project.

---
+++ ex01/rpn.mk
@@ -0,0 +1,8 @@
+override SRCSDIR	:= srcs/
+override MAINDIR    :=
+
+override SRCS		= $(addprefix $(SRCSDIR), $(SRC))
+
+SRC += $(addprefix $(MAINDIR), $(addsuffix .cpp, $(MAIN)))
+
+override MAIN := 	main \
