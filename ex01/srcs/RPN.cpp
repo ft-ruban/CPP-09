aa8be0f180ba0411ce86731e38d6da5e21fed7b7
Lea Devoudelle-Royer <ldevoude@z1r9p2.42lyon.fr>
2026-07-11 11:21:15 +0200

    feat(RPN.cpp): did a lot of progress, but I think I have an idea that imply making 2 stack instead of just one, but to try those ideas I really need to save the current state as I am unsure of the results.

--- ex01/srcs/RPN.cpp
+++ ex01/srcs/RPN.cpp
@@ -1,4 +1,5 @@
 #include "../includes/RPN.hpp"
+#include <stdexcept> //TODO mv to .hpp
 
 RPN::RPN(){
 
@@ -46,19 +47,122 @@ void RPN::getSizeOfInput(){
     std::cout<<size_of_input_<<std::endl;
 }
 
-static bool isNum(char c){
-    return(c >= '0' && c <= '9');
+
+
+static bool isSpace(char c){
+    return(c == ' ');
 }
 
+static bool isNum(char c){
+    std::cout<<"debug isNum c = "<<c<<" = "<<(c>='0' && c <= '9')<<std::endl;
+    return(c >= '0' && c <= '9');
+}
+//+ - / *
+static bool isValidToken(char c){
+    return(c == '+' || c == '-' || c == '/' || c == '*');
+}
+
+static void getToNextChar(std::stack<char>& input_){
+    input_.pop();
+    if(!isSpace(input_.top()))
+        throw std::invalid_argument("");
+    input_.pop();
+}
+//+ - / *
+// static int expression(int variable_x, int variable_y, char active_token){
+
+//     switch(active_token){
+//         case '+':
+//             return(variable_x + variable_y);
+//             break;
+//         case '-':
+//             return(variable_x - variable_y);
+//             break;
+//         case '/':
+//             if(variable_x == 0 || variable_y == 0)
+//                 throw std::invalid_argument("");
+//             return(variable_x / variable_y);
+//             break;
+//         case '*':
+//             return(variable_x * variable_y);
+//         default:
+//             throw std::invalid_argument("");
+//     }
+// }
+
+static void expression(std::stack<int>& buff_stack, char active_token){
+    int variable_x = 0;
+    int variable_y = 0;
+    
+    variable_x  = buff_stack.top();
+    buff_stack.pop();
+    variable_y = buff_stack.top();
+    buff_stack.pop();
+    std::cout<<"test"<<std::endl; //TODL
+    std::cout<<"active_token = "<<active_token<<std::endl; //TODL
+
+
+    switch(active_token){
+            case '+':
+                buff_stack.push(variable_x + variable_y);
+                break;
+            case '-':
+                buff_stack.push(variable_x - variable_y);
+                break;
+            case '/':
+                if(variable_y == 0)
+                    throw std::invalid_argument("");
+                buff_stack.push(variable_x / variable_y);
+                break;
+            case '*':
+                buff_stack.push(variable_x * variable_y);
+                break;
+            default:
+                throw std::invalid_argument("");
+        }   
+}
+
+//./bin/RPN "7 2 * 7 - 7"
+//$> ./RPN "1 2 * 2 / 2 * 2 4 - +"    Pour ce cas specifique il faut faire gaffe. a la fin on a dans la pile [2] on add [2,2,4] et on fait 2-4 PAS 4-2
 void RPN::calculusTreatment(){
+    std::stack<int> buff_stack;
+    int buff_stack_size = 0;
     char buff_one;
     char buff_two;
+    char buff_three;
 
+    // int variable_x = 0;
+    // int variable_y = 0;
+    char active_token;
+
+//maybe there is a way without usin buff one two and such
     buff_one = input_.top();
-    input_.pop();
-    input_.pop();
+    getToNextChar(input_);
     buff_two = input_.top();
-    input_.pop();
-    std::cout<<"num_one = "<<buff_one<<" num_2 = "<<buff_two<<std::endl;
-    std::cout<<"num_one is num? : "<< isNum(buff_one)<<"num_two is num? : "<<isNum(buff_two)<<std::endl;
+    getToNextChar(input_);
+    buff_three = input_.top();
+    getToNextChar(input_);
+
+    if(!isNum(buff_one) || !isNum(buff_two) || !isValidToken(buff_three)){
+        throw std::invalid_argument("");
+    }
+    buff_stack.push(buff_one - '0');
+    buff_stack_size++;
+    buff_stack.push(buff_two - '0');
+    buff_stack_size++;
+
+    active_token = buff_three;
+
+    expression(buff_stack, active_token);
+    // std::cout<<"Variable_x = "<< variable_x<<std::endl;
+    std::stack<int> buff = buff_stack;
+    int size_buff = 1;
+    while(size_buff != 0){
+        std::cout<<buff.top();
+        buff.pop();
+        size_buff--;
+    }
+    std::cout<<std::endl;
+
+    // std::cout<<"num_one is num? : "<< isNum(buff_one)<<"num_two is num? : "<<isNum(buff_two)<<std::endl;
 }
